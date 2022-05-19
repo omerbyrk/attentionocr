056dda685f5916d07d796340c464207515ba49ec
Ömer Bayrak <omersocial8@gmail.com>
2022-05-19 17:38:30 +0300

    Update download_texts.py

--- synthetic/download_texts.py
+++ synthetic/download_texts.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         response = requests.get('https://en.wikipedia.org/api/rest_v1/page/random/summary').json()
         text = response['extract'].strip()
         if len(text) > 32:
-            with open('texts/%d.txt' % index, 'wt') as f:
+            with open('/content/attentionocr/synthetic/texts/%d.txt' % index, 'wt') as f:
                 f.write(text)
