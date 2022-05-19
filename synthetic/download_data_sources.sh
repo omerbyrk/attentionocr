55fc672e1a9da1dda96961ab603623e7edf756d6
Ömer Bayrak <omersocial8@gmail.com>
2022-05-19 17:17:27 +0300

    Update download_data_sources.sh

--- synthetic/download_data_sources.sh
+++ synthetic/download_data_sources.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 if [ ! -d fonts ]; then
+  mkdir -p /content/attentionocr/synthetic/fonts
   git clone https://github.com/google/fonts.git fonts
-  find fonts -type f -iname "*.ttf" -exec mv {} fonts \;
+  find fonts -type f -iname "*.ttf" -exec mv {}  /content/attentionocr/synthetic/fonts;
 fi
 
 if [ ! -d texts ]; then
-  mkdir -p texts
-  python3 download_texts.py
+  mkdir -p /content/attentionocr/synthetic/texts
+  python3 /content/attentionocr/synthetic/texts/download_texts.py
 fi
 
 if [ ! -d images ]; then
-  mkdir -p images
-  wget https://thewallpaper.co//wp-content/uploads/2016/10preview/free-desktop-backgrounds-1920x1200-background-photos-windows-apple-mac-wallpapers-tablet-artworks-high-definition-free-1920x1200.jpg -O images/1.jpg
-  wget https://www.hdwallback.net/wp-content/uploads/2018/01/Awesome-Night-Time-Wide-Hd-New-Best-Desktop-Background-Full-Free-Hd-Wallpaper-Artworks-Desktop-Images-For-Apple.jpg -O images/2.jpg
+  mkdir -p /content/attentionocr/synthetic/images
+  wget https://icdn.ensonhaber.com/resimler/galeri/206457.jpg -O /content/attentionocr/synthetic/images/1.jpg
+  wget https://indigodergisi.com/wp-content/uploads/2015/06/dilara-dora-e1435562682497.jpg -O /content/attentionocr/synthetic/images/2.jpg
 fi
